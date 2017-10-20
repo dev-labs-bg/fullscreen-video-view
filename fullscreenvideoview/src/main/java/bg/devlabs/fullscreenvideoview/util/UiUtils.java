e734f05c0d213ba67da0e2462a5db0bc7e2c1133
Slavi Petrov <slavi@devlabs.bg>
2017-10-20 16:27:12 +0300

    Refactor getAllChildViews function Changed the documentation of the getAllChildViews methods

--- fullscreenvideoview/src/main/java/bg/devlabs/fullscreenvideoview/util/UiUtils.java
+++ fullscreenvideoview/src/main/java/bg/devlabs/fullscreenvideoview/util/UiUtils.java
@@ -1,9 +1,11 @@
 package bg.devlabs.fullscreenvideoview.util;
 
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.ViewGroup;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import bg.devlabs.fullscreenvideoview.FullscreenVideoView;
@@ -18,71 +20,77 @@ import static android.view.View.VISIBLE;
  */
 public class UiUtils {
     /**
-     * Shows all views except the parent layout.
+     * Shows all views except the parent layout
      *
      * @param parentLayout the top layout in the XML file
      */
     public static void showOtherViews(ViewGroup parentLayout) {
         List<View> views = getAllChildViews(parentLayout);
         int size = views.size();
-        for (int i = 1; i < size; i++) {
+        for (int i = 0; i < size; i++) {
             View view = views.get(i);
-            if (view instanceof FullscreenVideoView) {
-                continue;
-            }
-
             view.setVisibility(VISIBLE);
         }
     }
 
     /**
-     * Hides all views except the parent layout.
+     * Hides all views except the parent layout
      *
      * @param parentLayout the top layout in the XML file
      */
     public static void hideOtherViews(ViewGroup parentLayout) {
         List<View> views = getAllChildViews(parentLayout);
         int size = views.size();
-        for (int i = 1; i < size; i++) {
+        for (int i = 0; i < size; i++) {
             View view = views.get(i);
-            if (view instanceof FullscreenVideoView) {
-                continue;
-            }
-
             view.setVisibility(GONE);
         }
     }
 
     /**
-     * Get all child views from the parent layout except the VideoView.
+     * Search recursively through all children of the parent layout and checks their class.
+     * If they are ViewGroup classes, continues the recursion,
+     * if they are View classes, terminates the recursion
      *
-     * @param parentLayout the top layout in the XML file
-     * @return a list child views
+     * Used in {@link #hideOtherViews(ViewGroup)} to get all the Views that should be hidden
+     * Used in {@link #showOtherViews(ViewGroup)} to get all the Views that should be shown
+     *
+     * @param parentLayout the top layout in XML file
+     * @return a list of all non-ViewGroup views from the parent layout except the VideoView,
+     * but including Toolbar
      */
-    private static List<View> getAllChildViews(ViewGroup parentLayout) {
-        List<View> visited = new ArrayList<>();
-        List<View> unvisited = new ArrayList<>();
-        unvisited.add(parentLayout);
-
-        while (!unvisited.isEmpty()) {
-            View child = unvisited.remove(0);
-            visited.add(child);
-
-            if (child instanceof FullscreenVideoView) {
-                continue;
-            }
-
-            if (!(child instanceof ViewGroup)) {
-                continue;
-            }
-
-            ViewGroup group = (ViewGroup) child;
-            final int childCount = group.getChildCount();
-            for (int i = 0; i < childCount; i++) {
-                unvisited.add(group.getChildAt(i));
-            }
+    private static List<View> getAllChildViews(View parentLayout) {
+        if (!shouldCheckChildren(parentLayout)) {
+            return Collections.singletonList(parentLayout);
         }
 
-        return visited;
+        int childCount = ((ViewGroup) parentLayout).getChildCount();
+        List<View> children = new ArrayList<>(childCount);
+        for (int i = 0; i < childCount; i++) {
+            View view = ((ViewGroup) parentLayout).getChildAt(i);
+            if (shouldCheckChildren(view)) {
+                children.addAll(getAllChildViews(view));
+            } else {
+                if (!(view instanceof FullscreenVideoView)) {
+                    children.add(view);
+                }
+            }
+        }
+        return children;
+    }
+
+    /**
+     * Check if a view has children to iterate
+     *
+     * Used in {@link #getAllChildViews(View)} as a terminating case
+     *
+     * @param view the {@link View} that should be checked
+     * @return true if the View is a ViewGroup, but not FullscreenVideoView or Toolbar
+     */
+    private static boolean shouldCheckChildren(View view) {
+        return view instanceof ViewGroup &&
+                !(view instanceof Toolbar) &&
+                !(view instanceof android.widget.Toolbar) &&
+                !(view instanceof FullscreenVideoView);
     }
 }
