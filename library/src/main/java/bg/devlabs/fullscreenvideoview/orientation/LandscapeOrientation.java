17d1ded42c382fc15fa2bdbfc97cc8bf7ea2443f
Slavi Petrov <slavi@devlabs.bg>
2017-11-14 13:54:48 +0200

    Fix wrong value for LandscapeOrientation.REVERSE

--- library/src/main/java/bg/devlabs/fullscreenvideoview/orientation/LandscapeOrientation.java
+++ library/src/main/java/bg/devlabs/fullscreenvideoview/orientation/LandscapeOrientation.java
@@ -1,6 +1,7 @@
 package bg.devlabs.fullscreenvideoview.orientation;
 
 import static android.content.pm.ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE;
+import static android.content.pm.ActivityInfo.SCREEN_ORIENTATION_REVERSE_LANDSCAPE;
 import static android.content.pm.ActivityInfo.SCREEN_ORIENTATION_SENSOR_LANDSCAPE;
 import static android.content.pm.ActivityInfo.SCREEN_ORIENTATION_USER_LANDSCAPE;
 
@@ -13,7 +14,7 @@ import static android.content.pm.ActivityInfo.SCREEN_ORIENTATION_USER_LANDSCAPE;
 public enum LandscapeOrientation {
     SENSOR(SCREEN_ORIENTATION_SENSOR_LANDSCAPE),
     DEFAULT(SCREEN_ORIENTATION_LANDSCAPE),
-    REVERSE(SCREEN_ORIENTATION_LANDSCAPE),
+    REVERSE(SCREEN_ORIENTATION_REVERSE_LANDSCAPE),
     USER(SCREEN_ORIENTATION_USER_LANDSCAPE);
 
     private final int value;
