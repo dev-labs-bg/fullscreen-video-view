8c7452be39f2a478a88e036ceb6aa629d8d3700e
Slavi Petrov <slavi@devlabs.bg>
2019-11-18 16:16:04 +0200

    Fixed not calculating the margins when going fullscreen

---
+++ library/src/main/java/bg/devlabs/fullscreenvideoview/model/Margins.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2017 Dev Labs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package bg.devlabs.fullscreenvideoview.model;
+
+/**
+ * Created by Slavi Petrov on 18.11.2019
+ * Dev Labs
+ * slavi@devlabs.bg
+ */
+public class Margins {
+
+    private int left;
+    private int top;
+    private int right;
+    private int bottom;
+
+    public Margins(int left, int top, int right, int bottom) {
+        this.left = left;
+        this.top = top;
+        this.right = right;
+        this.bottom = bottom;
+    }
+
+    public int getLeft() {
+        return left;
+    }
+
+    public int getTop() {
+        return top;
+    }
+
+    public int getRight() {
+        return right;
+    }
+
+    public int getBottom() {
+        return bottom;
+    }
+}
