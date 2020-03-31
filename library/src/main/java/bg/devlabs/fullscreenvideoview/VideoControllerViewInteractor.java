4443236df868f3ceb18bd7430445340ad4c5e9b3
Slavi Petrov <slavi@devlabs.bg>
2020-03-31 12:33:34 +0300

    Change the verb form of the methods documentation in VideoControllerViewInteractor interface

--- library/src/main/java/bg/devlabs/fullscreenvideoview/VideoControllerViewInteractor.java
+++ library/src/main/java/bg/devlabs/fullscreenvideoview/VideoControllerViewInteractor.java
@@ -6,12 +6,12 @@ package bg.devlabs.fullscreenvideoview;
 interface VideoControllerViewInteractor {
 
     /**
-     * Remove the controller from the screen.
+     * Removes the controller from the screen.
      */
     void hide();
 
     /**
-     * Show the controller on screen. It will go away automatically after 'timeout' milliseconds
+     * Shows the controller on screen. It will go away automatically after 'timeout' milliseconds
      * of inactivity.
      *
      * @param timeout The timeout in milliseconds. Use 0 to show the controller
@@ -20,80 +20,80 @@ interface VideoControllerViewInteractor {
     void show(int timeout);
 
     /**
-     * Set progress to the SeekBar.
+     * Sets progress to the SeekBar.
      *
      * @return progress current position
      */
     int setProgress();
 
     /**
-     * Check if the SeekBar is currently being dragged.
+     * Checks if the SeekBar is currently being dragged.
      *
      * @return true if it's dragging, otherwise false
      */
     boolean isDragging();
 
     /**
-     * Check if the controller is showing.
+     * Checks if the controller is showing.
      *
      * @return true if it's showing, otherwise false
      */
     boolean isShowing();
 
     /**
-     * Check if the player is playing.
+     * Checks if the player is playing.
      *
      * @return true if it's playing, otherwise false
      */
     boolean isPlaying();
 
     /**
-     * A setter for the isDragging value.
+     * Changes the isDragging value.
      *
      * @param isDragging indicates if the progress bar is being dragged or not.
      */
     void setIsDragging(boolean isDragging);
 
     /**
-     * Update the pause/play drawable of the video controller.
+     * Updates the pause/play drawable of the video controller.
      */
     void updatePausePlay();
 
     /**
-     * Refresh the progress bar.
+     * Refreshes the progress bar.
      */
     void refreshProgress();
 
     /**
-     * Get the video duration.
+     * Gets the video duration.
      *
      * @return the video duration
      */
     int getDuration();
 
     /**
-     * Seek to a preferred position.
+     * Seeks to a preferred position.
      *
      * @param position the selected position
      */
     void seekTo(int position);
 
     /**
-     * Update the current time.
+     * Changes the current time.
      *
      * @param position the selected position
      */
     void setCurrentTime(int position);
 
     /**
-     * Update SeekBar progress.
+     * Updates the SeekBar progress.
      *
      * @param position the selected position
      */
     void updateSeekBarProgress(long position);
 
     /**
-     * Hide the video thumbnail image.
+     * Hides the video thumbnail image.
      */
     void hideThumbnail();
 }
